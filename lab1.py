36c0166021d52091009159d2721e26e958885666
AndriyBandurak <45455783+AndriyBandurak@users.noreply.github.com>
2018-11-29 13:36:54 +0200

    Create lab1.py

---
+++ lab1.py
@@ -0,0 +1,11 @@
+kurs="Програмування на мові Python "
+lab="Lab 1"
+name="Bandurak Andrii"
+koma=", "
+grupa="ПММ-2"
+vyklad="Savka I"
+print(kurs+lab)
+print(name+koma+grupa)
+print((vyklad+koma)*44+vyklad)
+result=((((123-23.1)*((pow(2,3))+2.2))/(127.24 -21.1)) -3.2
+print("Результат :",result)
