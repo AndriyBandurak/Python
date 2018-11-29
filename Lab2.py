224cd64382e2732ca13816e3409d4ef588cceb2c
AndriyBandurak <45455783+AndriyBandurak@users.noreply.github.com>
2018-11-29 21:29:37 +0700

    Create Lab2.py

---
+++ Lab2.py
@@ -0,0 +1,8 @@
+print('Створення прикладних програм на мові Python, Lab2')
+print('Бандурак Андрій, Num2')
+print('Введи:')
+x = float(input('x = '))
+y = float(input('y = '))
+z = float(input('z = '))
+result = ((((((z-23.1)*((pow(2,x)+2.2)))/(y-21.1))-3.2
+print('Результат: ', result)
